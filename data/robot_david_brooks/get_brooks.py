e3af6d6c1d332d1b0dfef96d186299ef902d0e21
montoyjh <montoyjh@gmail.com>
2017-07-11 12:01:24 -0700

    removed ipynb and other non file

--- data/robot_david_brooks/get_brooks.py
+++
@@ -1,6 +0,0 @@
-from nytimesarticle import articleAPI
-
-api = articleAPI("1d21881c9f374736821cd81efbffa68b")
-
-if __name__=='__main__':
-    data = api.search(q="David Brooks", fq={"byline":"David Brooks"})
